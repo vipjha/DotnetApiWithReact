54c43af6192790c279c59703c6446c0ea39e0e1e
Vj <vipinjha@hotmail.com>
2022-09-01 02:42:45 +0530

    Section2End

---
+++ RestoreApi/API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+           // return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            #pragma warning disable CS8604 // Possible null reference argument.
+            return await context.Products.FindAsync(id);
+            #pragma warning restore CS8604 // Possible null reference argument.
+            // return Ok(product);
+        }
+    }
+}
--- RestoreApi/API/Program.cs
+++ RestoreApi/API/Program.cs
@@ -1,3 +1,6 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add by Vipin
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initalizer(context);
+}
+catch (Exception ex)
+{
+    throw;
+   // Logging CreateLogger(ex, "Problem in migrate data");
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
